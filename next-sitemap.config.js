3120e1128bfbe2ec8bfd2367326484b6a1794bc8
Huỳnh Hữu Nghĩa <nghiabatuoidev@gmail.com>
2024-12-01 11:54:14 +0700

    update site-map 2

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     siteUrl: 'https://www.phelieutailocvina.com/',
+    siteUrl: 'https://phelieutailocvina.com/',
     generateRobotsTxt: true, // Tạo tệp robots.txt
     sitemapSize: 7000,
   };
